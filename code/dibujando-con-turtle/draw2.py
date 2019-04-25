e6c3eab8e6a3d2a7574982d752e967958d461e00
avdata99 <andres@data99.com.ar>
2019-04-24 23:44:39 -0300

    jugando con dibujos

---
+++ code/dibujando-con-turtle/draw2.py
@@ -0,0 +1,20 @@
+""" requerimientos
+instalar python3-tk
+Linux: 
+ - sudo apt-get install python3-tk
+"""
+import turtle 
+
+# sets window to 200x200 pixels, in upper left of screen
+turtle.setup(width=400, height=400, startx=0, starty=0)
+turtle.colormode(255)
+turtle.title("Welcome to the turtle zoo!")
+
+turtle.goto(-100, -120)
+
+for x in range(1, 240):
+    turtle.forward(150 - (0.6 * x))
+    turtle.left(64)
+    turtle.pencolor(x % 255, 2 * x % 255, 3 * x % 255)
+
+turtle.done()
