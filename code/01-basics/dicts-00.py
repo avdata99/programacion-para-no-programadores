0431269a4f2e03d5d64298d1f87b7bedb067ff99
Andres Vazquez <andres@data99.com.ar>
2022-02-05 12:08:39 -0300

    Dicts

---
+++ code/01-basics/dicts-00.py
@@ -0,0 +1,10 @@
+data = {
+    'edad': 32,
+    'nombre': 'Juan'
+}
+
+nombre = data['nombre']
+edad = data['edad']
+print(f'Nombre: {nombre}, edad {edad} años')
+
+# Nombre: Juan, edad 32 años
--- code/01-basics/dicts-01.py
+++ code/01-basics/dicts-01.py
@@ -1,9 +1,23 @@
 
 data = {
     'edad': 32,
-    'nombre': 'Juan'
+    'nombre': 'Juan',
+    'educacion': {
+        'secundario': 'Monserrat',
+        'universidad': 'UNC',
+    }
 }
 
 nombre = data['nombre']
 edad = data['edad']
-print(f'Nombre: {nombre}, edad {edad} años')
+universidad = data['educacion']['universidad']
+
+print(f'Nombre: {nombre}, edad {edad} años. Universidad: {universidad}')
+# Nombre: Juan, edad 32 años. Universidad: UNC
+
+# Agregarle datos
+data['ocupacion'] = 'Desarrollador'
+data['educacion']['primario'] = 'San Juan'
+
+print(data)
+# {'edad': 32, 'nombre': 'Juan', 'educacion': {'secundario': 'Monserrat', 'universidad': 'UNC', 'primario': 'San Juan'}, 'ocupacion': 'Desarrollador'}
