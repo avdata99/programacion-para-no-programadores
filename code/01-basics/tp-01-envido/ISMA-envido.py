2fb37bf33bca009f02d1419619b9a702fbedb087
Andres Vazquez <andres@data99.com.ar>
2022-02-13 21:53:35 -0300

    Isma

---
+++ code/01-basics/tp-01-envido/ISMA-envido.py
@@ -0,0 +1,70 @@
+
+palos={
+    'Oro':'1',
+    'basto':'2',
+    'Espada':'3',
+    'Copa':'4',
+
+}
+figura=[10,11,12]
+
+palo=[0,0,0,0]
+cartas=[0,0,0,0]
+
+
+
+print("ingrese el numero de la primera carta")
+numero=int(input())
+
+print(f"ingrese el palo segun corresponda:")
+for x in palos:
+    print(x,":",palos[x])
+
+posicion=int(input())
+palo[posicion-1]+=1
+
+if numero in figura:
+    cartas[posicion-1]+=0
+else:
+    cartas[posicion-1]+=numero
+
+print("ingrese el numero de la segunda carta")
+numero=int(input())
+
+print(f"ingrese el palo segun corresponda:")
+for x in palos:
+    print(x,":",palos[x])
+
+posicion=int(input())
+palo[posicion-1]+=1
+
+if numero in figura:
+    cartas[posicion-1]+=0
+else:
+    cartas[posicion-1]+=numero
+
+print("ingrese el numero de la tercera carta")
+numero=int(input())
+
+
+print(f"ingrese el palo segun corresponda:")
+for x in palos:
+    print(x,":",palos[x])
+
+posicion=int(input())
+palo[posicion-1]+=1
+
+if numero in figura:
+    cartas[posicion-1]+=0
+else:
+    cartas[posicion-1]+=numero
+
+
+if max(palo)>1:
+    tanto=max(cartas)+20
+else:
+   tanto=max(cartas)
+print(f"tiene {tanto} tantos para el envido")
+
+palo=[0,0,0,0]
+cartas=[0,0,0,0]
