8c915c1392bcd70ae8a6323600008fd9e5bc17d5
Andres Vazquez <andres@data99.com.ar>
2022-03-10 19:05:31 -0300

    Change name

--- code/01-basics/fraccion-ejercicio.py
+++ code/01-basics/fraccion-ejercicio.py
@@ -4,5 +4,8 @@ class Fraccion:
     #    validar que "otro" es una fraccion
     #    sumar y devolver un objecto NUEVO tiṕo fraccion
 
-assert Fraccion(2,3) + Fraccion(1,3) == Fraccion(3,3)
-assert Fraccion(4,5) + Fraccion(3,5) == Fraccion(7,5)
+assert Fraccion(2, 3) + Fraccion(1, 3) == Fraccion(1, 1)
+assert Fraccion(4, 5) + Fraccion(3, 5) == Fraccion(7, 5)
+assert Fraccion(4, 5) + Fraccion(6, 5) == Fraccion(2, 1)
+assert Fraccion(5, 12) + Fraccion(4, 19) == Fraccion(143, 228)
+assert Fraccion(3, 2) + Fraccion(8, 11) == Fraccion(49, 22)
--- code/01-basics/fraccion.py
+++
@@ -1,8 +0,0 @@
-class Fraccion:
-    # init con numerador y denominador
-    # def __add__ ....
-    #    validar que "otro" es una fraccion
-    #    sumar y devolver un objecto NUEVO tiṕo fraccion
-
-assert Fraccion(2,3) + Fraccion(1,3) == Fraccion(3,3)
-assert Fraccion(4,5) + Fraccion(3,5) == Fraccion(7,5)
