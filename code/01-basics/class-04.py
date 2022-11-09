a4f2bcd9bc8ba1747fd8046c6bd55807e7704a7d
avdata99 <andres@data99.com.ar>
2022-11-09 19:34:17 -0300

    Mejorando clase

--- code/01-basics/class-04.py
+++ code/01-basics/class-04.py
@@ -1,7 +1,16 @@
 class Carta:
     def __init__(self, numero, palo):
         self._numero = numero
-        self._palo = palo
+        self._palo = self._validar_palo(palo)
+        self.palos_validos = ['oro', 'copa', 'basto', 'espada']
+
+    def _validar_palo(self, value):
+        """ Devuelve el valor limpio o lanza un error si no es un palo valido """
+        if type(value) != str:
+            raise Exception ('Solo están permitidas letras')
+        if value.lower() not in self.palos_validos:
+            raise Exception('Palo invalido')
+        return value.lower()
 
     @property
     def numero(self):
@@ -19,9 +28,7 @@ class Carta:
 
     @palo.setter
     def palo(self, value):
-        if type(value) != str:
-            raise Exception ('Solo están permitidas letras')
-        self._palo = value
+        self._palo = self._validar_palo(value)
 
     def __str__(self):
         return f'{self.numero} de {self.palo}'
@@ -30,3 +37,5 @@ class Carta:
 
 carta1 = Carta(3, 'espada')
 print(str(carta1))
+
+carta1.palo = 'Pooo'
