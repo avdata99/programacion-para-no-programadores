c3d84785201dd06fa6d59f80bbbfe60f4db1010e
avdata99 <andres@data99.com.ar>
2022-11-09 19:23:08 -0300

    Corrigiendo palo setter

--- code/01-basics/class-04.py
+++ code/01-basics/class-04.py
@@ -11,10 +11,7 @@ class Carta:
     def numero(self, value):
         if type(value) != int:
             raise Exception('Solo están permitidos numeros')
-        elif value > 0 and value <= 12:
-            self._numero = value
-        else:
-            raise Exception('Solo están permitidos numeros del 1 al 12')
+        self._numero = value
 
     @property
     def palo(self):
@@ -22,6 +19,8 @@ class Carta:
 
     @palo.setter
     def palo(self, value):
+        if type(value) != str:
+            raise Exception ('Solo están permitidas letras')
         self._palo = value
 
     def __str__(self):
@@ -31,5 +30,3 @@ class Carta:
 
 carta1 = Carta(3, 'espada')
 print(str(carta1))
-
-carta1.numero = -17
