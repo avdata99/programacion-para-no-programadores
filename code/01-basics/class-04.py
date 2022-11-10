ae2d49291bd4a40a974071a116a2e2dbcb0ce185
avdata99 <andres@data99.com.ar>
2022-11-09 21:07:16 -0300

    Jugando

--- code/01-basics/class-04.py
+++ code/01-basics/class-04.py
@@ -2,6 +2,9 @@
 Clase Carta para juegos de cartas
 """
 
+from random import choice, randint
+
+
 class Carta:
     def __init__(self, numero, palo):
         self._numero = numero
@@ -28,8 +31,68 @@ class Carta:
     def __str__(self):
         return f'{self.numero} de {self.palo}'
 
+    def __repr__(self):
+        return self.__str__()
+
+    def __add__(self, otro):
+        if type(otro) == int:
+            return self._numero + otro
+            # nuevo_numero = self._numero + otro
+            # return Carta(nuevo_numero, self._palo)
+
+    def __eq__(self, otro):
+        if type(otro) == Carta:
+            if self._numero == otro.numero:
+                if self._palo == otro.palo:
+                    return True
+            return False
+        elif type(otro) == int:
+            return self._numero == otro
+        elif type(otro) == str:
+            return self._palo == otro
+
+
+class Mazo:
+    def __init__(self):
+        self._cartas = []
+    
+    def append(self, value):
+        if type(value) != Carta:
+            raise Exception('Solo cartas')
+        self._cartas.append(value)
+
+    @property
+    def cartas(self):
+        return self._cartas
+    
+    def __str__(self):
+        return f'<Mazo {self._cartas}>'
+
+    def agregar_carta_al_azar(self):
+        while True:
+            numero = randint(1, 12)
+            palo = choice(['palo', 'oro', 'copa'])
+            carta = Carta(numero, palo)
+            if carta not in self._cartas:
+                self.append(carta)
+                break
+
+    def __len__(self):
+        return len(self._cartas)
+
+
 # Pruebas
 
-carta1 = Carta(3, 'espada')
-print(str(carta1))
-'3 de espada'
+carta1 = Carta(1, 'espada')
+carta2 = Carta(2, 'espada')
+
+mazo = Mazo()
+mazo.append(carta1)
+mazo.append(carta2)
+
+for n in range(30):
+    mazo.agregar_carta_al_azar()
+
+print(len(mazo))
+print(mazo)
+
