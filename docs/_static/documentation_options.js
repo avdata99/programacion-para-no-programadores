8ae56c34fcd8e5f6b1eee0c3bb01f08cccd9907d
avdata99 <andres@data99.com.ar>
2022-11-20 23:06:45 -0300

    Compilando

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v 0.4.6',
+    VERSION: 'v 0.4.7',
     LANGUAGE: 'es',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
